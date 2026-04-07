f95e7a669603feddeabfdb0688a1b003e4c4318c
s980024 <s980024@student.knoxschools.org>
2026-04-07 20:29:07 +0000

    more stuff for data

--- get_data/fetch_data.py
+++ get_data/fetch_data.py
@@ -1,11 +1,17 @@
 from ucimlrepo import fetch_ucirepo 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
+def load_heart_data():
+    #copied directly from UCI Machine Learning Repository package documentation 
+    # fetch dataset 
+    heart_disease = fetch_ucirepo(id=45) 
   
-# fetch dataset 
-heart_disease = fetch_ucirepo(id=45) 
-  
-# data (as pandas dataframes) 
-X = heart_disease.data.features 
-y = heart_disease.data.targets 
+    # data (as pandas dataframes) 
+    X = heart_disease.data.features 
+    y = heart_disease.data.targets 
   
 # metadata 
 print(heart_disease.metadata) 
