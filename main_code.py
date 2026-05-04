9b0fd1142c43f4d2f84a66ca7f9113e2f0ad3ae7
s980024 <s980024@student.knoxschools.org>
2026-05-04 15:29:14 +0000

    code_review

--- main_code.py
+++ main_code.py
@@ -9,6 +9,12 @@ class Person():
     def test(self):
         return self.age
 
+    def info():
+        self.input = input("Would you like some info on prevention of heart disease or to keep diseases from getting worse?")
+        while self.input == yes or self.input == Yes:
+            #make lists of the info and then print random
+            print("If you are smoking it is recommended that you stop, as nicotine can narrow your blood vessels which is not good.")
+            self.input = input("Would you like more information?")
 
 
 
