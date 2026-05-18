c81ba8de6bebd5b8dd5cb378855413f32a497186
s980024 <s980024@student.knoxschools.org>
2026-05-18 00:08:52 +0000

    worksss

--- main_code.py
+++ main_code.py
@@ -18,7 +18,7 @@ class Person():
         return self.age
 
     def info(self):
-        self.input = input("Would you like some info on prevention of heart disease or to keep diseases from getting worse?")
+        self.input = input("Would you like some info on prevention of heart disease or to keep diseases from getting worse? ")
         self.list_info = ["If you are smoking it is recommended that you stop, as nicotine can narrow your blood vessels which is not good.", "It is recommended to change your diet to a low soidum/low fat diet and manage your weight.", "If you have not seen a doctor yet and your prediction says you have heart disease, it would be a good idea to schedule a doctor appointment.", "It is recommended to aim for at least of 150 minutes of moderate intensity of exercise once a week to try and keep it from getting worse.", "If you have a doctor and are on medications, it is recommended that you keep taking that medication until your doctor tells you not to.", "It is also good to get between 7-8 hours of sleep to help keep blood pressure low. "]
         self.i = 0
         if self.input == "yes" or self.input == "Yes":
@@ -28,7 +28,10 @@ class Person():
                 self.list_info.pop(self.i)
                 self.i += 1
                 input_break = input("Would you like more information? ")
-                if input_break == "no" or input_break == "No":
+                if self.i == len(self.list_info):
+                    print("That is all the information I have for you.")
+                    break
+                elif input_break == "no" or input_break == "No":
                     break
 
 
